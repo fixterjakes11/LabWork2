730832c9e7c4e277f3947e07f88f7bb0f596e0e4
fixterjakes11 <rahmetovcom06@gmail.com>
2022-02-22 15:00:01 +0300

    Добавьте файлы проекта.

---
+++ LabWork2/ButtflyController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace LabWork2
+{
+    public delegate void Print(string message);
+    class ButtflyController
+    {
+        public List<Butterfly> ButterFly { get; private set; }
+
+        public ButtflyController()
+        {
+            ButterFly = new List<Butterfly>();
+        }
+        /// <summary>
+        /// Добавление в список бабочек
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="color"></param>
+        /// <param name="age"></param>
+        /// <param name="eat"></param>
+        public void Add(string type, string color, string age, string eat)
+        {
+            ButterFly.Add(new Butterfly
+            {
+                Type = type,
+                Color = color,
+                Eat = eat,
+                Age = age
+
+            }) ;
+        }
+        /// <summary>
+        /// Вывод списка бабочек с помощью делегата
+        /// </summary>
+        /// <param name="print"></param>
+        public void PrintButtfly(Print print)
+        {
+            foreach(var i in ButterFly)
+            {
+                print($"Имя или тип бабочки: {i.Type}- цвет: {i.Color} - срок жизни: {i.Age} - основная пища: {i.Eat}");
+            }
+        }
+
+
+    }
+}
---
+++ LabWork2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LabWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Butterfly butterfly = new Butterfly("Бабочка", "Red",  "Капуста");
+            Console.WriteLine(butterfly.PrintValues());
+
+            Butterfly butterfly1 = new Butterfly("Бабочка", "Blue", "Цветы", "12 day");
+            Console.WriteLine(butterfly1.PrintValues());
+
+            ButtflyController controller = new ButtflyController();
+            controller.Add("Бабочка", "Blue", "12 дней", "капуста");
+
+ 
+            Print print = MyPrint;
+            controller.PrintButtfly(print);
+            Console.WriteLine(Butterfly.number());
+        }
+
+        private static void MyPrint(string message)
+        {
+            Console.WriteLine(message); ;
+        }
+    }
+}
