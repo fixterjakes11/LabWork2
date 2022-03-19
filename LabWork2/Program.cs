0b7d88c1f99376df04853bfb46f23626c7d3e0ad
fixterjakes11 <rahmetovcom06@gmail.com>
2022-03-19 18:46:53 +0300

    1

--- LabWork2/Program.cs
+++ LabWork2/Program.cs
@@ -8,18 +8,11 @@ namespace LabWork2
     {
         static void Main(string[] args)
         {
-            Butterfly butterfly = new Butterfly("Бабочка", "Red",  "Капуста");
+            Butterfly butterfly = new Butterfly("Бабочка", "Red");
             Console.WriteLine(butterfly.PrintValues());
 
             Butterfly butterfly1 = new Butterfly("Бабочка", "Blue", "Цветы", "12 day");
             Console.WriteLine(butterfly1.PrintValues());
-
-            ButtflyController controller = new ButtflyController();
-            controller.Add("Бабочка", "Blue", "12 дней", "капуста");
-
- 
-            Print print = MyPrint;
-            controller.PrintButtfly(print);
             Console.WriteLine(Butterfly.number());
         }
 
@@ -27,5 +20,25 @@ namespace LabWork2
         {
             Console.WriteLine(message); ;
         }
+
+
+        public static void AddButfly()
+        {
+            ButtflyController controller = new ButtflyController();
+            Console.WriteLine("Type: ");
+            string type = Console.ReadLine();
+            Console.WriteLine("Color: ");
+            string color = Console.ReadLine();
+            Console.WriteLine("Day: ");
+            string day = Console.ReadLine();
+            Console.WriteLine("Eat: ");
+            string eat = Console.ReadLine();
+
+            controller.Add(type, color, day, eat);
+            Print print = MyPrint;
+            controller.PrintButtfly(print);
+           
+        }
+
     }
 }
